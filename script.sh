1fcc78305de507485624f6f04b45869f891809e6
suhailroushan13 <suhailroushan13@gmail.com>
2022-09-19 16:34:00 +0000

    First Commit

---
+++ script.sh
@@ -0,0 +1,10 @@
+git pull origin master
+git log --oneline | sed -n '2p' > log.txt
+var=$(cat log.txt | head -n1 | awk '{print $1;}')
+echo $var "Is your previous Commit ID"
+git reset $var
+git reset --soft HEAD@{1}
+git commit -m "Revert"
+git reset --hard
+git push -f
+
