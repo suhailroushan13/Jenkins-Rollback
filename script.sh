93a61d462d8d94d1d3181ea9d7a11d8548295d76
suhailroushan13 <suhailroushan13@gmail.com>
2022-09-19 16:40:57 +0000

    Fourth Commit

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 git pull origin master
+git log --oneline > log.txt
+cat log.txt
 git log --oneline | sed -n '2p' > log.txt
 var=$(cat log.txt | head -n1 | awk '{print $1;}')
 echo $var "Is your previous Commit ID"
@@ -7,4 +9,3 @@ git reset --soft HEAD@{1}
 git commit -m "Revert"
 git reset --hard
 git push -f
-
